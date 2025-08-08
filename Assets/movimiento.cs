cc24565f4a53543e380a40409a95299f2110988a
ROITMAN LUCIANO IVAN <49261770@ort>
2025-08-08 12:13:11 -0300

    movimiento tercera persona

---
+++ Assets/movimiento.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v) * speed * Time.deltaTime;
+        transform.Translate(move);
+    }
+}
