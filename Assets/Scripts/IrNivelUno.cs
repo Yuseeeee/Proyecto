57e9d3fed68a5f36a9f78d1467425607cb63eda6
Yuseeeee <49379138@est.ort.edu.ar>
2025-08-08 12:11:41 -0300

    casi bien

--- Assets/Scripts/IrNivelUno.cs
+++ Assets/Scripts/IrNivelUno.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class IrNivelUno : MonoBehaviour
+public class CambioNivel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string Nivel1;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Jugador"))
+        {
+            SceneManager.LoadScene(Nivel1);
+        }
     }
 }
