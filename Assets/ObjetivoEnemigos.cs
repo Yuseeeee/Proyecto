4c02bc427120845d28c04f12a971c66020c1ed40
Yuseeeee <49379138@est.ort.edu.ar>
2025-08-22 10:40:11 -0300

    a

---
+++ Assets/ObjetivoEnemigos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AgenteCapsula : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    public Transform objetivo;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = objetivo.position;
+    }
+}
