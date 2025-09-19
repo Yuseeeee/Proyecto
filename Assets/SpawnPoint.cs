843aa75d3b20f4082aa21e880814d73cdeb002f8
LucianRoitman <49261770@est.ort.edu.ar>
2025-09-19 11:07:40 -0300

    El spawn

---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject objetoASpawnear;  // El prefab
+    public Transform puntoDeSpawn;      // El lugar donde aparecerá
+
+    void Start()
+    {
+        // Instancia el objeto en el punto de spawn
+        Instantiate(objetoASpawnear, puntoDeSpawn.position, puntoDeSpawn.rotation);
+    }
+}
