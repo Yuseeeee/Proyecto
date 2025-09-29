0791bef91a16f986708059a4f7839702c32228b7
Yuseeeee <49379138@est.ort.edu.ar>
2025-09-29 10:23:24 -0300

    Arreglo

--- Assets/SpawnPoint.cs
+++ Assets/SpawnPoint.cs
@@ -1,13 +1,13 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
 
-public class SpawnPoint : MonoBehaviour
-{
-    public GameObject objetoASpawnear;  // El prefab
-    public Transform puntoDeSpawn;      // El lugar donde aparecerá
+//public class SpawnPoint : MonoBehaviour
+//{
+ //   public GameObject objetoASpawnear;  // El prefab
+ //   public Transform puntoDeSpawn;      // El lugar donde aparecerá
 
-    void Start()
-    {
+   // void Start()
+  //  {
         // Instancia el objeto en el punto de spawn
-        Instantiate(objetoASpawnear, puntoDeSpawn.position, puntoDeSpawn.rotation);
-    }
-}
+  //      Instantiate(objetoASpawnear, puntoDeSpawn.position, puntoDeSpawn.rotation);
+   // }
+//}
