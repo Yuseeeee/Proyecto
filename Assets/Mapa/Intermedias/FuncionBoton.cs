76dfcf8393e6944a14a4377f5b77a04c7bf7d493
LucianRoitman <49261770@est.ort.edu.ar>
2025-10-17 10:28:50 -0300

    BotonMenuTerminado

---
+++ Assets/Mapa/Intermedias/FuncionBoton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class FuncionBoton : MonoBehaviour
+{
+    public Button boton; 
+    public KeyCode tecla = KeyCode.M;
+    public string NombreDeEscena;
+
+    private void Start()
+    {
+        boton.onClick.AddListener(CargarEscena);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(tecla))
+        {
+            CargarEscena();
+        }
+    }
+    void CargarEscena()
+    {
+        SceneManager.LoadScene(NombreDeEscena);
+    }
+}
