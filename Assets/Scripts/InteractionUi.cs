afd1c039d0b228c844b882715c8c48ca7c543434
LucianRoitman <49261770@est.ort.edu.ar>
2025-10-21 12:08:39 -0300

    interaction2

---
+++ Assets/Scripts/InteractionUi.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Interaction : MonoBehaviour
+{
+    public GameObject avisoUI;   
+    public KeyCode teclaInteraction = KeyCode.E;
+    private bool enRango = false;
+
+    void Start()
+    {
+        avisoUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (enRango)
+        {
+            avisoUI.SetActive(true);
+
+            if (Input.GetKeyDown(teclaInteraction))
+            {
+                Debug.Log("Interaccion");
+            }
+        }
+        else
+        {
+            avisoUI.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enRango = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enRango = false;
+        }
+    }
+}
