16213bcaffdfdaf0b62811f47f9162e0623f743e
LucianRoitman <49261770@est.ort.edu.ar>
2025-11-10 10:38:06 -0300

    beje

---
+++ Assets/CambioDeEscena.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioDeEscena : MonoBehaviour
+{
+    public string MapaPrevio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(MapaPrevio);
+        }
+    }
+}
