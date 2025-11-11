bd5ea73857c70f7f87c3c1fa3136d27d2be00016
LucianRoitman <49261770@est.ort.edu.ar>
2025-11-11 12:08:15 -0300

    Selected

---
+++ Assets/Mapa/Intermedias/Scripts/Selected.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selected : MonoBehaviour
+{
+    LayerMask Mask;
+    public float distancia = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mask = LayerMask.GetMask("Rayvast Detect");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distancia, Mask))
+        {
+            if(hit.collider.tag == "Objeto interactivo")
+            {
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    hit.collider.transform.GetComponent<ObjetoInteractivo>().ActivarObjeto();
+                }
+            }
+        }
+    }
+}
