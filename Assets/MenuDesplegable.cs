45475729db720fa348636f5df39147e99c36da9f
LucianRoitman <49261770@est.ort.edu.ar>
2025-12-10 13:47:28 -0300

    menu desplegable

---
+++ Assets/MenuDesplegable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuDesplegable : MonoBehaviour
+{
+    public GameObject[] botones;
+    private bool abierto = false;
+
+    public void ToggleMenu()
+    {
+        abierto = !abierto;
+
+        foreach (GameObject b in botones)
+        {
+            b.SetActive(abierto);
+        }
+    }
+}
