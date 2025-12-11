4be5214a012ccbdefecf075f27428892649d4d53
LucianRoitman <49261770@est.ort.edu.ar>
2025-12-11 11:14:05 -0300

    salto, boceto 2 parkour

---
+++ Assets/Scripts/SaltoPersonaje.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaltoPersonaje : MonoBehaviour
+{
+    public float jumpForce = 7f;
+    public Rigidbody rb;
+    private bool enSuelo = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && enSuelo)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            enSuelo = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Cada vez que toca el piso, puede saltar de nuevo
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            enSuelo = true;
+        }
+    }
+}
