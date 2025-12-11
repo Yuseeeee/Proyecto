00bd9751938ca611754eb7625ce94ab506888449
Yuseeeee <49379138@est.ort.edu.ar>
2025-12-11 09:56:32 -0300

    puerta y llave

---
+++ Assets/Scripts/Llave.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Llave : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        AgarrarLlave inv = other.GetComponent<AgarrarLlave>();
+        if (inv != null)
+        {
+            inv.RecogerLlave();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Puerta.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
+public class Puerta : MonoBehaviour
+{
+    public TMP_Text mensajeLlave;
+    public float duracionMensaje = 2f;
+
+    private bool mostrandoMensaje = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        AgarrarLlave inventario = other.GetComponent<AgarrarLlave>();
+
+        if (inventario.tieneLlave)
+        {
+            SceneManager.LoadScene("Inicio");
+        }
+        else
+        {
+            if (!mostrandoMensaje)
+                StartCoroutine(MostrarMensaje());
+        }
+    }
+
+    private IEnumerator MostrarMensaje()
+    {
+        mostrandoMensaje = true;
+
+        mensajeLlave.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(duracionMensaje);
+
+        mensajeLlave.gameObject.SetActive(false);
+
+        mostrandoMensaje = false;
+    }
+}
